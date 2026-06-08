ce6d9c30218539ad78e3e3abf3d370bd67a5b27d
Miguel Castro <Miguel.Castro@parceiros.nos.pt>
2026-06-08 14:30:51 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // /:project is the project landing page
+  // / is the projects list, if there is only one project: it is the same as /:project
+  {
+    path: '',
+    loadComponent: () =>
+      import('./modules/projects-list/projects-list').then((m) => m.ProjectsList),
+  },
+];
---
+++ src/app/modules/projects-list/projects-list.ts
@@ -0,0 +1,15 @@
+import { httpResource } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+
+@Component({
+  selector: 'app-projects-list',
+  imports: [],
+  templateUrl: './projects-list.html',
+  styleUrl: './projects-list.css',
+})
+export class ProjectsList {
+  projects = httpResource<Project[]>(() => environment.projectsUrl);
+
+  constructor() {}
+}
